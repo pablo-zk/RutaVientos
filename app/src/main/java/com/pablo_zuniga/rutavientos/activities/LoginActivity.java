091380c43ab7106f1ed9b091fc0c20f7fe237d88
Pablo-Zuniga <pablousky44@gmail.com>
2022-02-02 09:43:18 +0100

    Activity para login y signup

---
+++ app/src/main/java/com/pablo_zuniga/rutavientos/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.pablo_zuniga.rutavientos.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pablo_zuniga.rutavientos.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
