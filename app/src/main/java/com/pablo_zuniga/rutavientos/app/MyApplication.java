a5e8478047422d2f340abff170c0cc55c03b591e
Pablo-Zuniga <pablousky44@gmail.com>
2022-02-03 08:53:28 +0100

    Creacion class MyApplication

--- app/src/main/java/com/pablo_zuniga/rutavientos/app/MyApplication.java
+++ app/src/main/java/com/pablo_zuniga/rutavientos/app/MyApplication.java
@@ -1,4 +1,44 @@
 package com.pablo_zuniga.rutavientos.app;
 
-public class MyApplication {
+import android.app.Application;
+
+import com.pablo_zuniga.rutavientos.models.Route;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
+public class MyApplication extends Application {
+    public static AtomicInteger routeId = new AtomicInteger();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setUpRealmConfig();
+        Realm realm = Realm.getDefaultInstance();
+        routeId = getIdByTable(realm, Route.class);
+        realm.close();
+    }
+
+    private void setUpRealmConfig(){
+        Realm.init(getApplicationContext());
+        RealmConfiguration config = new RealmConfiguration
+                .Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(config);
+    }
+
+    private <T extends RealmObject> AtomicInteger getIdByTable(Realm realm, Class<T> anyClass){
+        RealmResults<T> results = realm.where(anyClass).findAll();
+        if (results.size()>0){
+            return new AtomicInteger(results.max("id").intValue());
+        }
+        else{
+            return new AtomicInteger(0);
+        }
+    }
 }
