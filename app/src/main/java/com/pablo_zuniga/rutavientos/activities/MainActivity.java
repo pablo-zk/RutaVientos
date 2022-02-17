9c7f21c44e9230801d7f846e46c6a6ac2da7ea6a
gorkaaerdozaiin10 <71701933+gorkaaerdozaiin10@users.noreply.github.com>
2022-02-17 09:28:42 +0100

    Adding Recycler View

--- app/src/main/java/com/pablo_zuniga/rutavientos/activities/MainActivity.java
+++ app/src/main/java/com/pablo_zuniga/rutavientos/activities/MainActivity.java
@@ -62,14 +62,10 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabUnselected(TabLayout.Tab tab) {}
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabReselected(TabLayout.Tab tab) {}
         });
     }
 
