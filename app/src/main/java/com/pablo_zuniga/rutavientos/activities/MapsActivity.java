dc4571d9ddac286dfed805f91f52acd4950b3870
Pablo-Zuniga <pablousky44@gmail.com>
2022-02-21 21:48:23 +0100

    Implement methods map

--- app/src/main/java/com/pablo_zuniga/rutavientos/activities/MapsActivity.java
+++ app/src/main/java/com/pablo_zuniga/rutavientos/activities/MapsActivity.java
@@ -1,25 +1,42 @@
 package com.pablo_zuniga.rutavientos.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.pablo_zuniga.rutavientos.R;
 import com.pablo_zuniga.rutavientos.databinding.ActivityMapsBinding;
+import com.pablo_zuniga.rutavientos.fragments.CreateRoutesFragment;
+import com.pablo_zuniga.rutavientos.models.Route;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.CancellationException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private MarkerOptions marker;
+    private Boolean markerCreated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +62,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        markerCreated = false;
         mMap = googleMap;
-        //mMap.setMaxZoomPreference(10);
         mMap.setMinZoomPreference(12);
 
-        // Add a marker in Sydney and move the camera
+        //DEFAULT VIEW
         LatLng cuatrovientos = new LatLng(42.8242834,-1.659874);
-        //LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(cuatrovientos).title("Cuatrovientos"));
         CameraPosition camera = new CameraPosition.Builder().target(cuatrovientos).zoom(18).tilt(15).build();
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(camera));
+
+        //EVENTS
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                if(markerCreated){
+                    showAlert();
+                    return;
+                }
+                markerCreated = true;
+                marker = new MarkerOptions();
+                marker.position(latLng);
+                marker.title("This is Origin");
+                marker.draggable(true);
+                mMap.addMarker(marker);
+
+                Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
+                List<Address> matches = null;
+                try {
+                    matches = geoCoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Address address = matches.get(0);
+                String addressText = String.format("%s", address.getAddressLine(0));
+                Toast.makeText(MapsActivity.this,addressText, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                marker.hideInfoWindow();
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                marker.showInfoWindow();
+                Toast.makeText(MapsActivity.this,"Marker Dragged to \n"+
+                        "Lat: "+marker.getPosition().latitude+"\n"+
+                        "Lng: "+marker.getPosition().longitude, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void showAlert(){
+        new AlertDialog.Builder(this)
+                .setTitle("ALERTA")
+                .setMessage("Ya has seleccionado una ubicación. Si quieres cambiar de ubicacíon mantenga pulsado el marcador y muévalo a donde quiera.")
+                .setPositiveButton("OK",null)
+                .show();
     }
 }
