00dee2206524fd5b0a28ff6e6f85872abc817094
Eltuco19 <asierelorza18@gmail.com>
2022-02-22 20:29:46 +0100

    Cositis

--- app/src/main/java/com/pablo_zuniga/rutavientos/activities/RouteDetails.java
+++ app/src/main/java/com/pablo_zuniga/rutavientos/activities/RouteDetails.java
@@ -3,6 +3,7 @@ package com.pablo_zuniga.rutavientos.activities;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ public class RouteDetails extends AppCompatActivity {
     Button btnApuntarse;
 
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,25 +44,39 @@ public class RouteDetails extends AppCompatActivity {
             routeActual = realm.where(Route.class).equalTo("id",bundle.getInt("id")).findFirst();
             userOfRoute = realm.where(User.class).equalTo("id",routeActual.getDriver()).findFirst();
             userCurrent = realm.where(User.class).equalTo("isActive",true).findFirst();
-        }
-        //TODO partir de aquí poner los datos en los txt o lo que haya en la view
-
+       }
         btnApuntarse = (Button) findViewById(R.id.btnApuntarse);
-        if(userCurrent.getRoutesId().contains(routeActual.getId())){
-            btnApuntarse.setText("Desapuntarse");
-        }else{
-            btnApuntarse.setText("Apuntarse");
-        }
-        btnApuntarse.setOnClickListener(view -> {
+        if (userCurrent.getId() == userOfRoute.getId()){
+            btnApuntarse.setText("Eliminar");
+            btnApuntarse.setBackground(getDrawable(R.drawable.button_del));
+        } else {
+            //TODO partir de aquí poner los datos en los txt o lo que haya en la view
             if(userCurrent.getRoutesId().contains(routeActual.getId())){
-                showInfoAlert("¿Seguro que quieres desapuntarte de la ruta " + routeActual.getOrigin() + "-" + routeActual.getDestiny() + " a las " + routeActual.getDateHour() + "?");
+                btnApuntarse.setText("Desapuntarse");
             }else{
-                if(routeActual.getFreeSeats() == 0){
-                    Toast.makeText(getBaseContext(),"ERROR: No quedan plazas disponibles", Toast.LENGTH_SHORT).show();
+                btnApuntarse.setText("Apuntarse");
+            }
+        }
+
+
+        btnApuntarse.setOnClickListener(view -> {
+            if (btnApuntarse.getText().toString() == "Eliminar"){
+                showInfoAlert("¿Seguro que quieres eliminar la ruta?");
+
+            } else {
+                if(userCurrent.getRoutesId().contains(routeActual.getId())){
+                    showInfoAlert("¿Seguro que quieres desapuntarte de la ruta " + routeActual.getOrigin() + "-" + routeActual.getDestiny() + " a las " + routeActual.getDateHour() + "?");
                 }else{
-                    showInfoAlert("¿Seguro que quieres apuntarte de la ruta " + routeActual.getOrigin() + "-" + routeActual.getDestiny() + " a las " + routeActual.getDateHour() + "?");
+                    if(routeActual.getFreeSeats() == 0){
+                        Toast.makeText(getBaseContext(),"ERROR: No quedan plazas disponibles", Toast.LENGTH_SHORT).show();
+                    }else{
+                        showInfoAlert("¿Seguro que quieres apuntarte de la ruta " + routeActual.getOrigin() + "-" + routeActual.getDestiny() + " a las " + routeActual.getDateHour() + "?");
+
+                    }
                 }
             }
+
+
         });
 
     }
@@ -70,23 +86,35 @@ public class RouteDetails extends AppCompatActivity {
                 .setTitle("CONFIRMACIÓN")
                 .setMessage(message)
                 .setPositiveButton("Ok", (dialogInterface, i) -> {
-                    realm.beginTransaction();
-                    if(message.contains("desapuntarte")){
-                        userCurrent.getRoutesId().remove(userCurrent.getRoutesId().indexOf(routeActual.getId()));
-                        routeActual.setFreeSeats(routeActual.getFreeSeats() + 1);
-                        btnApuntarse.setText("Apuntarse");
-                    }else{
-                        userCurrent.getRoutesId().add(routeActual.getId());
-                        routeActual.setFreeSeats(routeActual.getFreeSeats() - 1);
-                        btnApuntarse.setText("Desapuntarse");
+                    if (message.contains("eliminar")){
+                        realm.beginTransaction();
+                        routeActual.deleteFromRealm();
+                        realm.commitTransaction();
+                        Toast.makeText(getBaseContext(),"Ruta eliminada con exito", Toast.LENGTH_SHORT).show();
+                    } else {
+                        realm.beginTransaction();
+                        if(message.contains("desapuntarte")){
+                            userCurrent.getRoutesId().remove(userCurrent.getRoutesId().indexOf(routeActual.getId()));
+                            routeActual.setFreeSeats(routeActual.getFreeSeats() + 1);
+                            btnApuntarse.setText("apuntarte");
+                            Toast.makeText(getBaseContext(),"Te has desapuntado con exito", Toast.LENGTH_SHORT).show();
+                        }else{
+                            userCurrent.getRoutesId().add(routeActual.getId());
+                            routeActual.setFreeSeats(routeActual.getFreeSeats() - 1);
+                            btnApuntarse.setText("Desapuntarse");
+                            Toast.makeText(getBaseContext(),"Te has apuntado con exito", Toast.LENGTH_SHORT).show();
+                        }
+                        realm.copyToRealmOrUpdate(userCurrent);
+                        realm.commitTransaction();
+                        realm.beginTransaction();
+                        realm.copyToRealmOrUpdate(routeActual);
+                        realm.commitTransaction();
                     }
-                    realm.copyToRealmOrUpdate(userCurrent);
-                    realm.commitTransaction();
-                    realm.beginTransaction();
-                    realm.copyToRealmOrUpdate(routeActual);
-                    realm.commitTransaction();
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
                 })
                 .setNegativeButton("Cancel",null)
                 .show();
+
     }
 }
